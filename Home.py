1b05b9199eace97dcc20a3ada938fd1d95c72d81
Fede-Rausa <126892947+Fede-Rausa@users.noreply.github.com>
2026-05-29 14:24:17 +0200

    Restore imports and dataset loading functionality

--- Home.py
+++ Home.py
@@ -32,6 +32,18 @@ st.write('Insights e strumenti per analizzare il rischio di abbandono')
 if "dataset" not in st.session_state:
     with st.spinner("Caricamento del grosso dataset in corso..."):
         st.session_state["dataset"] = load_large_dataset()
-        st.session_state['clu_des'] = json.load(open('descrizioni.json', 'r'))
+        st.session_state['clu_des'] = [{'nome':'Talenti Disillusi', 'descrizione':'''
+Dipendenti che lasciano per insoddisfazione verso l'ambiente aziendale, poca crescita, comunicazione debole, decisioni lente o scarso supporto''', 'cluster_id':1},
+                                       {'nome':'Cercatori di Crescita', 'descrizione':'''
+Dimissioni volontarie legate al desiderio di crescita professionale, sviluppo di nuove competenze, cambio carriera o migliori opportunità
+''', 'cluster_id':2},
+                                       {'nome':'Uscite Consapevoli', 'descrizione':'''
+Uscite pianificate e ragionate, legate a obiettivi personali, timing professionale, cambiamenti di vita o nuove priorità individuali
+''', 'cluster_id':3},
+                                       {'nome':'Equilibri Fragili', 'descrizione':'''
+Dimissioni legate a conflitti nel team, difficoltà relazionali, scarsa comunicazione tra colleghi, stress o dinamiche di gruppo negative
+''', 'cluster_id':4}
+                                       ]
+        #json.load(open('streamlit_dashboard\\descrizioni.json', 'r'))
     st.success("Dataset caricato con successo!")
 
